13c1a32e04a4ce7153c9674e13d867e96f089868
Roy Brabson <brabson@alumni.duke.edu>
2023-11-27 20:01:00 -0500

    initial code drop;

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/AutoMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/AutoMecanumDrive.java
@@ -1,55 +1,302 @@
 package org.firstinspires.ftc.teamcode.roadrunner.drive;
 
+import static org.firstinspires.ftc.teamcode.roadrunner.drive.DriveConstants.MAX_ACCEL;
+import static org.firstinspires.ftc.teamcode.roadrunner.drive.DriveConstants.MAX_ANG_ACCEL;
+import static org.firstinspires.ftc.teamcode.roadrunner.drive.DriveConstants.MAX_ANG_VEL;
+import static org.firstinspires.ftc.teamcode.roadrunner.drive.DriveConstants.MAX_VEL;
+import static org.firstinspires.ftc.teamcode.roadrunner.drive.DriveConstants.TRACK_WIDTH;
+
 import androidx.annotation.NonNull;
 
+import com.acmerobotics.roadrunner.control.PIDCoefficients;
+import com.acmerobotics.roadrunner.drive.Drive;
+import com.acmerobotics.roadrunner.drive.DriveSignal;
+import com.acmerobotics.roadrunner.followers.HolonomicPIDVAFollower;
+import com.acmerobotics.roadrunner.followers.TrajectoryFollower;
+import com.acmerobotics.roadrunner.kinematics.Kinematics;
+import com.acmerobotics.roadrunner.kinematics.MecanumKinematics;
 import com.acmerobotics.roadrunner.localization.Localizer;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
+import com.acmerobotics.roadrunner.trajectory.constraints.AngularVelocityConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.MecanumVelocityConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.MinVelocityConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.ProfileAccelerationConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryAccelerationConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryVelocityConstraint;
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.mechanisms.MecanumDrive;
 import org.firstinspires.ftc.teamcode.roadrunner.localizer.ThreeWheelLocalizer;
+import org.firstinspires.ftc.teamcode.roadrunner.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.teamcode.roadrunner.trajectorysequence.TrajectorySequenceBuilder;
+import org.firstinspires.ftc.teamcode.roadrunner.trajectorysequence.TrajectorySequenceRunner;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class AutoMecanumDrive extends com.acmerobotics.roadrunner.drive.MecanumDrive{
+/**
+ * Autonomous mecanum drive.
+ */
+public class AutoMecanumDrive extends Drive {
+    public static double VX_WEIGHT = 1;
+    public static double VY_WEIGHT = 1;
+    public static double OMEGA_WEIGHT = 1;
     private Localizer localizer;
     private final MecanumDrive drive = new MecanumDrive("drive", "Mecanum Drive");
+
+    private double kV;
+    private double kA;
+    private double kStatic;
+    private double trackWidth;
+    private double wheelBase;
+    private double lateralMultiplier;
+
+    private VoltageSensor batteryVoltageSensor;
+
+    private TrajectorySequenceRunner trajectorySequenceRunner;
+    private TrajectoryFollower follower;
+    private List<Integer> lastEncPositions = new ArrayList<>();
+    private List<Integer> lastEncVels = new ArrayList<>();
+    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(0, 0, 0);
+    public static PIDCoefficients HEADING_PID = new PIDCoefficients(0, 0, 0);
+    private static final TrajectoryVelocityConstraint VEL_CONSTRAINT = getVelocityConstraint(MAX_VEL, MAX_ANG_VEL, TRACK_WIDTH);
+    private static final TrajectoryAccelerationConstraint ACCEL_CONSTRAINT = getAccelerationConstraint(MAX_ACCEL);
+
+    /**
+     * Creates a new autonomous mecanum drive.
+     */
     public AutoMecanumDrive() {
-        super(DriveConstants.kV, DriveConstants.kA, DriveConstants.kStatic, DriveConstants.TRACK_WIDTH, DriveConstants.WHEEL_RADIUS);
+        this.kV = DriveConstants.kV;
+        this.kA = DriveConstants.kA;
+        this.kStatic = DriveConstants.kStatic;
+        this.trackWidth = DriveConstants.TRACK_WIDTH;
+        this.wheelBase = DriveConstants.WHEEL_BASE;
+        this.lateralMultiplier = DriveConstants.LATERAL_MULTIPLIER;
     }
 
+    /**
+     * Initializes the autonomous mecanum drive.
+     * @param hardwareMap the hardware map for the robot.
+     * @param telemetry the telemetry to use when sending data to the drive station.
+     */
     public void init(HardwareMap hardwareMap, Telemetry telemetry) {
         drive.init(hardwareMap);
         localizer = new ThreeWheelLocalizer(hardwareMap);
-        setLocalizer(localizer);
+
+        batteryVoltageSensor = hardwareMap.voltageSensor.iterator().next();
+        for (LynxModule module : hardwareMap.getAll(LynxModule.class)) {
+            module.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
+        }
+
+        List<Integer> lastTrackingEncPositions = new ArrayList<>();
+        List<Integer> lastTrackingEncVels = new ArrayList<>();
+        trajectorySequenceRunner = new TrajectorySequenceRunner(
+                follower, HEADING_PID, batteryVoltageSensor,
+                lastEncPositions, lastEncVels, lastTrackingEncPositions, lastTrackingEncVels
+        );
+
+        follower = new HolonomicPIDVAFollower(TRANSLATIONAL_PID, TRANSLATIONAL_PID, HEADING_PID,
+                new Pose2d(0.5, 0.5, Math.toRadians(5.0)), 0.5);
     }
 
+    @NonNull
+    @Override
+    public Localizer getLocalizer() {
+        return localizer;
+    }
+
+    @Override
+    public void setLocalizer(@NonNull Localizer localizer) {
+        this.localizer = localizer;
+    }
+
+    /**
+     * Unused when using three-wheel odometry.
+     * @return <code>0.0</code>.
+     */
     @Override
     protected double getRawExternalHeading() {
         return 0;
     }
 
+    /**
+     * Gets the current position of the wheels. This is the number of inches the wheels have
+     * traveled.
+     *
+     * @return a list of wheel positions.
+     */
     @NonNull
-    @Override
     public List<Double> getWheelPositions() {
-        List<Double> wheelPositions = new ArrayList<>();
-
-        // TODO: Should match the four wheels used in setting the wheel power
-
-        return wheelPositions;
+        return Arrays.asList(0.0, 0.0, 0.0, 0.0);
     }
 
+    /**
+     * Returns the wheel velocities, or <code>null</code> if the localizer doesn't calculate
+     * wheel velocities.
+     * @return the wheel velocities, or <code>null</code> if the wheel velocities aren't calcualted
+     *         by the localizer.
+     */
     public List<Double> getWheelVelocities() {
-        List<Double> wheelVelocities = new ArrayList<>();
-
-        // TODO: Should match the four wheels used setting the wheel power
-
-        return wheelVelocities;
+        return null;
     }
 
     @Override
-    public void setMotorPowers(double v, double v1, double v2, double v3) {
-        drive.setMotorPowers(v, v1,v2, v3);
+    public void setDrivePower(@NonNull Pose2d drivePower) {
+        List<Double> powers = MecanumKinematics.robotToWheelVelocities(
+                drivePower,
+                trackWidth,
+                wheelBase,
+                lateralMultiplier
+        );
+        setMotorPowers(powers.get(0), powers.get(1), powers.get(2), powers.get(3));
+    }
+
+    @Override
+    public void setDriveSignal(DriveSignal driveSignal) {
+        List<Double> velocities = MecanumKinematics.robotToWheelVelocities(
+                driveSignal.getVel(),
+                trackWidth,
+                wheelBase,
+                lateralMultiplier
+        );
+        List<Double> accelerations = MecanumKinematics.robotToWheelAccelerations(
+                driveSignal.getAccel(),
+                trackWidth,
+                wheelBase,
+                lateralMultiplier
+        );
+        List<Double> powers = Kinematics.calculateMotorFeedforward(velocities, accelerations, kV, kA, kStatic);
+        setMotorPowers(powers.get(0), powers.get(1), powers.get(2), powers.get(3));
+    }
+
+    /**
+     * Sets the power for the drive motors.
+     * @param leftFront the power for the left front motor.
+     * @param leftRear the power for the left rear motor.
+     * @param rightRear the power for the right rear motor.
+     * @param rightFront the power for the right front motor.
+     */
+    public void setMotorPowers(double leftFront, double leftRear, double rightRear, double rightFront) {
+        drive.setMotorPowers(leftFront, leftRear,rightRear, rightFront);
+    }
+
+    public void update() {
+        updatePoseEstimate();
+        DriveSignal signal = trajectorySequenceRunner.update(getPoseEstimate(), getPoseVelocity());
+        if (signal != null) setDriveSignal(signal);
+    }
+
+    public TrajectoryBuilder trajectoryBuilder(Pose2d startPose) {
+        return new TrajectoryBuilder(startPose, VEL_CONSTRAINT, ACCEL_CONSTRAINT);
+    }
+
+    public TrajectoryBuilder trajectoryBuilder(Pose2d startPose, boolean reversed) {
+        return new TrajectoryBuilder(startPose, reversed, VEL_CONSTRAINT, ACCEL_CONSTRAINT);
+    }
+
+    public TrajectoryBuilder trajectoryBuilder(Pose2d startPose, double startHeading) {
+        return new TrajectoryBuilder(startPose, startHeading, VEL_CONSTRAINT, ACCEL_CONSTRAINT);
+    }
+
+    public TrajectorySequenceBuilder trajectorySequenceBuilder(Pose2d startPose) {
+        return new TrajectorySequenceBuilder(
+                startPose,
+                VEL_CONSTRAINT, ACCEL_CONSTRAINT,
+                MAX_ANG_VEL, MAX_ANG_ACCEL
+        );
+    }
+
+    public void turnAsync(double angle) {
+        trajectorySequenceRunner.followTrajectorySequenceAsync(
+                trajectorySequenceBuilder(getPoseEstimate())
+                        .turn(angle)
+                        .build()
+        );
+    }
+
+    public void turn(double angle) {
+        turnAsync(angle);
+        waitForIdle();
+    }
+
+    public void followTrajectoryAsync(Trajectory trajectory) {
+        trajectorySequenceRunner.followTrajectorySequenceAsync(
+                trajectorySequenceBuilder(trajectory.start())
+                        .addTrajectory(trajectory)
+                        .build()
+        );
+    }
+
+    public void followTrajectory(Trajectory trajectory) {
+        followTrajectoryAsync(trajectory);
+        waitForIdle();
+    }
+
+    public void followTrajectorySequenceAsync(TrajectorySequence trajectorySequence) {
+        trajectorySequenceRunner.followTrajectorySequenceAsync(trajectorySequence);
+    }
+
+    public void followTrajectorySequence(TrajectorySequence trajectorySequence) {
+        followTrajectorySequenceAsync(trajectorySequence);
+        waitForIdle();
+    }
+
+    public Pose2d getLastError() {
+        return trajectorySequenceRunner.getLastPoseError();
+    }
+
+    public void waitForIdle() {
+        while (!Thread.currentThread().isInterrupted() && isBusy())
+            update();
+    }
+
+    public boolean isBusy() {
+        return trajectorySequenceRunner.isBusy();
+    }
+
+    public void setPIDFCoefficients(DcMotor.RunMode runMode, PIDFCoefficients coefficients) {
+        PIDFCoefficients compensatedCoefficients = new PIDFCoefficients(
+                coefficients.p, coefficients.i, coefficients.d,
+                coefficients.f * 12 / batteryVoltageSensor.getVoltage()
+        );
+    }
+
+    public void setWeightedDrivePower(Pose2d drivePower) {
+        Pose2d vel = drivePower;
+
+        if (Math.abs(drivePower.getX()) + Math.abs(drivePower.getY())
+                + Math.abs(drivePower.getHeading()) > 1) {
+            // re-normalize the powers according to the weights
+            double denom = VX_WEIGHT * Math.abs(drivePower.getX())
+                    + VY_WEIGHT * Math.abs(drivePower.getY())
+                    + OMEGA_WEIGHT * Math.abs(drivePower.getHeading());
+
+            vel = new Pose2d(
+                    VX_WEIGHT * drivePower.getX(),
+                    VY_WEIGHT * drivePower.getY(),
+                    OMEGA_WEIGHT * drivePower.getHeading()
+            ).div(denom);
+        }
+
+        setDrivePower(vel);
+    }
+
+    public static TrajectoryVelocityConstraint getVelocityConstraint(double maxVel, double maxAngularVel, double trackWidth) {
+        return new MinVelocityConstraint(Arrays.asList(
+                new AngularVelocityConstraint(maxAngularVel),
+                new MecanumVelocityConstraint(maxVel, trackWidth)
+        ));
+    }
+
+    public static TrajectoryAccelerationConstraint getAccelerationConstraint(double maxAccel) {
+        return new ProfileAccelerationConstraint(maxAccel);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
@@ -49,6 +49,8 @@ public class DriveConstants {
     public static double WHEEL_RADIUS = 1.8898; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
     public static double TRACK_WIDTH = 12; // in
+    public static double WHEEL_BASE = 12; // in
+    public static double LATERAL_MULTIPLIER = 1.0;
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
