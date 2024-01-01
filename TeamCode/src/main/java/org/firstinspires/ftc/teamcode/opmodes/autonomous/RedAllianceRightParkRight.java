5962b9af5b978ef638f026cd97e44d9497d66316
Roy Brabson <brabson@alumni.duke.edu>
2024-01-01 16:02:36 -0500

    tuning of roadrunner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/RedAllianceRightParkRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/RedAllianceRightParkRight.java
@@ -14,11 +14,11 @@ import org.firstinspires.ftc.teamcode.processors.FirstVisionProcessor;
 import org.firstinspires.ftc.vision.VisionPortal;
 
 @Autonomous(name = "Red Alliance Right", group ="Active")
-public class RedAllianceRight extends LinearOpMode {
-
+public class RedAllianceRightParkRight extends LinearOpMode {
     private AutoMecanumDrive drive;
     private FirstVisionProcessor visionProcessor;
     private VisionPortal visionPortal;
+
     @Override
     public void runOpMode() throws InterruptedException {
         // Setup to send telemetry data to the FTC Dashboard
@@ -31,6 +31,7 @@ public class RedAllianceRight extends LinearOpMode {
         // Get the spike mark with the team prop
         visionProcessor = new FirstVisionProcessor();
         visionPortal = VisionPortal.easyCreateWithDefaults(hardwareMap.get(WebcamName.class, "Webcam 1"), visionProcessor);
+        sleep(3 * 1000);
         FirstVisionProcessor.Selected selection = visionProcessor.getSelection();
         telemetry.addData("Spike Mark", selection);
 
@@ -51,7 +52,7 @@ public class RedAllianceRight extends LinearOpMode {
             case MIDDLE:
                 telemetry.addLine("Right Spike Mark");
                 telemetry.update();
-                executeRedAllianceStartRightCenterSpike();
+                middleSpikeMark();
                 break;
             case NONE:
                 telemetry.addLine("Team Prop Not Detected");
@@ -59,31 +60,60 @@ public class RedAllianceRight extends LinearOpMode {
         }
     }
 
-    private void executeRedAllianceStartRightCenterSpike() {
+    /**
+     * Drops the purple pixel off on the middle spike mark, maneuvers to the backdrop and scores
+     * the yellow pixel in the middle, and then parks the robot to the right side of the backdrop.
+     */
+    private void middleSpikeMark() {
+        /*
         // Set starting pose
-        Pose2d startingPose = new Pose2d(-35, -61, Math.toRadians(90));
+        Pose2d startingPose = new Pose2d(12, -63.5, Math.toRadians(90));
         drive.setPoseEstimate(startingPose);
 
         // Move to center spike mark
         Trajectory traj1 = drive.trajectoryBuilder(startingPose)
-                .forward(28)
+                .forward(12)
                 .build();
 
         // Move to center position on backdrop
         Trajectory traj2a = drive.trajectoryBuilder(traj1.end())
-                .back(10)
+                .back(5)
                 .build();
         Trajectory traj2b = drive.trajectoryBuilder(traj2a.end())
-                .strafeRight(12)
-                .splineTo(new Vector2d(48, -35), Math.toRadians(90))
+                .splineTo(new Vector2d(60, -130), Math.toRadians(225))
                 .build();
 
         // Park to the right of the backdrop
         Trajectory traj3a = drive.trajectoryBuilder(traj2b.end())
-                .strafeLeft(25)
+                .strafeRight(20)
                 .build();
         Trajectory traj3b = drive.trajectoryBuilder(traj3a.end())
+                .forward(10)
+                .build();
+         */
+
+        Pose2d startingPose = new Pose2d(0, 0, Math.toRadians(90));
+        drive.setPoseEstimate(startingPose);
+
+        // Move to center spike mark
+        Trajectory traj1 = drive.trajectoryBuilder(startingPose)
+                .forward(12)
+                .build();
+
+        // Move to center position on backdrop
+        Trajectory traj2a = drive.trajectoryBuilder(traj1.end())
                 .back(5)
+                .build();
+        Trajectory traj2b = drive.trajectoryBuilder(traj2a.end())
+                .splineTo(new Vector2d(25, -23), Math.toRadians(245))
+                .build();
+
+        // Park to the right of the backdrop
+        Trajectory traj3a = drive.trajectoryBuilder(traj2b.end())
+                .strafeRight(25)
+                .build();
+        Trajectory traj3b = drive.trajectoryBuilder(traj3a.end())
+                .forward(8)
                 .build();
 
         // Unpack the robot
@@ -93,19 +123,19 @@ public class RedAllianceRight extends LinearOpMode {
 
         // Drive to the spike mark and drop off the pixel
         drive.followTrajectory(traj1);
-        telemetry.addLine("Drop Off Yellow Pixel");
+        telemetry.addLine("Drop Off Purple Pixel");
         telemetry.update();
-        sleep(4 * 1000);
+        sleep(1000);
 
         // Drive to the backdrop, raise the arm and rotate the pixel container, and score the pixel
         drive.followTrajectory(traj2a);
         drive.followTrajectory(traj2b);
         telemetry.addLine("Raise Arm and Rotate Pixel Container");
         telemetry.update();
-        sleep(1*1000);
-        telemetry.addLine("Score Purple Pixel");
+        sleep(1000);
+        telemetry.addLine("Score Yellow Pixel");
         telemetry.update();
-        sleep(4 * 1000);
+        sleep(1000);
 
         // Park the robot on the right side of the backdrop
         telemetry.addLine("Return Arm/Container to Intake Position");
