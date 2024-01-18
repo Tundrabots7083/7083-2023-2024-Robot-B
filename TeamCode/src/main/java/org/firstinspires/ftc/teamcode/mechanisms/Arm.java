bc0fe1ffbeab66873d4b3d067df71e213c6877a0
Roy Brabson <brabson@alumni.duke.edu>
2024-01-17 22:39:16 -0500

    adjust flip servo values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Arm.java
@@ -32,13 +32,13 @@ public class Arm implements Mechanism {
     public static double CONTAINER_INTAKE_POS = 0.65;
     public static int ARM_SCORE_LOW_POS = 3000;
     public static int LIFT_SCORE_LOW_POS = 0;
-    public static double CONTAINER_SCORE_LOW_POS = 0.65;
+    public static double CONTAINER_SCORE_LOW_POS = 0.25;
     public static int ARM_SCORE_MIDDLE_POS = 2750;
     public static int LIFT_SCORE_MIDDLE_POS = 0;
-    public static double CONTAINER_SCORE_MIDDLE_POS = 0.65;
+    public static double CONTAINER_SCORE_MIDDLE_POS = 0.25;
     public static int ARM_SCORE_HIGH_POS = 2750;
     public static int LIFT_SCORE_HIGH_POS = 0;
-    public static double CONTAINER_SCORE_HIGH_POS = 0.65;
+    public static double CONTAINER_SCORE_HIGH_POS = 0.25;
     public static int ARM_START_POS = 0;
     public static int LIFT_START_POS = 0;
     public static double CONTAINER_START_POS = 0.65;
