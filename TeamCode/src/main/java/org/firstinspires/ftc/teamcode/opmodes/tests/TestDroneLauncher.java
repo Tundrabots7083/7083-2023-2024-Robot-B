414cbe0262187214dc0acc58d0f1d4db757b908f
Roy Brabson <brabson@alumni.duke.edu>
2024-01-17 19:01:04 -0500

    drone launcher test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/TestDroneLauncher.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.opmodes.tests;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.controls.DroneLauncherController;
+
+@TeleOp (name="Drone Launcher Test", group="test")
+public class TestDroneLauncher extends OpMode {
+    private DroneLauncherController droneLauncherController;
+    @Override
+    public void init() {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        droneLauncherController = new DroneLauncherController(hardwareMap, telemetry);
+        telemetry.addLine("Initialization Complete");
+        telemetry.update();
+    }
+
+    @Override
+    public void loop() {
+        droneLauncherController.execute(gamepad1, gamepad2, telemetry);
+    }
+}
+
