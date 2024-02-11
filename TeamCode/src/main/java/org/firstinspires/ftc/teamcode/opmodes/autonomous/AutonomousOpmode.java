58da7677c44483987e6298440ed3ef19e181a3fa
Roy Brabson <brabson@alumni.duke.edu>
2024-02-11 16:40:14 -0500

    autonomous refactoring

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/AutonomousOpmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/AutonomousOpmode.java
@@ -1,6 +1,6 @@
 package org.firstinspires.ftc.teamcode.opmodes.autonomous;
 
 public interface AutonomousOpmode {
-    static long DELAY_BACKSTAGE_IN_MILLIS = 0;
-    static long DELAY_FRONTSTAGE_IN_MILLIS = 3000;
+    long DELAY_BACKSTAGE_IN_MILLIS = 0;
+    long DELAY_FRONTSTAGE_IN_MILLIS = 3000;
 }
