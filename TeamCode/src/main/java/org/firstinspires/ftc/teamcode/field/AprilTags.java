bdd47fcefa9a4cf7e1619a83a8b893bed7cc6a75
Roy Brabson <brabson@alumni.duke.edu>
2024-02-13 13:28:42 -0500

    distance sensor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/field/AprilTags.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.field;
+
+/**
+ * April tags on the backdrop.
+ */
+public interface AprilTags {
+    public int BLUE_ALLIANCE_LEFT = 1;
+    public int BLUE_ALLIANCE_CENTER = 2;
+    public int BLUE_ALLIANCE_RIGHT = 3;
+    public int RED_ALLIANCE_LEFT = 4;
+    public int RED_ALLIANCE_CENTER = 5;
+    public int RED_ALLIANCE_RIGHT = 6;
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sensors/DistanceSensor.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode.sensors;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+/**
+ * Obtains the distance from the sensor to the object in front of the sensor.
+ */
+public class DistanceSensor {
+    private final Telemetry telemetry;
+    private com.qualcomm.robotcore.hardware.DistanceSensor distanceSensor;
+
+    /**
+     * Creates a DistanceSensor object from the included hardware map. The distance sensor must be
+     * named `distanceSensor`.
+     * @param hardwareMap the hardware map for the robot.
+     * @param telemetry the telemetry to be used for any output to the driver station.
+     */
+    public DistanceSensor(HardwareMap hardwareMap, Telemetry telemetry) {
+        this.telemetry = telemetry;
+        distanceSensor = hardwareMap.get(com.qualcomm.robotcore.hardware.DistanceSensor.class, "distanceSensor");
+    }
+
+    /**
+     * Returns the current distance in inches
+     * @return the current distance sas measured by the sensor. If no reading is available
+     *         (perhaps the sensor is out of range), then distanceOutOfRange is returned;
+     */
+    public double getDistance() {
+        return getDistance(DistanceUnit.INCH);
+    }
+
+    /**
+     * Returns the current distance in the indicated distance units
+     * @param du the unit of distance in which the result should be returned
+     * @return the current distance sas measured by the sensor. If no reading is available
+     *         (perhaps the sensor is out of range), then distanceOutOfRange is returned;
+     */
+    public double getDistance(DistanceUnit du) {
+        return distanceSensor.getDistance(du);
+    }
+}
