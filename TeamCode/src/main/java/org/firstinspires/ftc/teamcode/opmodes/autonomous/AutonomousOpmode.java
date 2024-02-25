3ef42d4de08b2b5da388972ef739d7b228a374c0
Roy Brabson <rbrabson@gmail.com>
2024-02-24 21:28:51 -0500

    increase frontstage delay to 5 seconds

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/AutonomousOpmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/AutonomousOpmode.java
@@ -6,5 +6,5 @@ import com.acmerobotics.dashboard.config.Config;
 public interface AutonomousOpmode {
     long DELAY_BACKSTAGE = 0;
     long DELAY_PARK_AT_BACKDROP = 6000;
-    long DELAY_FRONTSTAGE = 3000;
+    long DELAY_FRONTSTAGE = 5000;
 }
