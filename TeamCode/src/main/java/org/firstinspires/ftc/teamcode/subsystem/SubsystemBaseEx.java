83fb1015c2b8e4f078b7ac55ffa818e6abe335e5
Roy Brabson <rbrabson@gmail.com>
2024-07-22 16:51:17 -0400

    Include custom subsystem base

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/SubsystemBaseEx.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/SubsystemBaseEx.java
@@ -2,7 +2,18 @@ package org.firstinspires.ftc.teamcode.subsystem;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
 
+/**
+ * Base subsystem used by our subsystems. This extends the FTCLib subsystem so that includes some
+ * methods used by multiple subsystems.
+ */
 public abstract class SubsystemBaseEx extends SubsystemBase implements SubsystemEx {
+
+    /**
+     * Modifies the power value so that it is between the minimum and maximum values.
+     * @param power
+     * @param minPower
+     * @return
+     */
     protected double modifyMotorPower(double power, double minPower) {
         // Cap the motor power at 1 and -1
         power = Math.max(-1, Math.min(1, power));
